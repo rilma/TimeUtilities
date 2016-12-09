ea464f2fd5e7835a206fbdb9b5cc2c899c2c2a5b
rilma <rri5@cornell.edu>
2016-12-08 20:09:26 -0500

    MLT added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 if __name__ == '__main__':
 
     setup( name='timeutil', \
-        version='1.0.0', \
+        version='2.0.0', \
         description='Utilitities to deal with time-series lists', \
         author='Ronald Ilma', \
         author_email='rri5@cornell.edu', \
