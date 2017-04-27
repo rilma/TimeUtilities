df90094beeab480de1ee6d6c2c806cedbfd5ebd8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-27 10:43:48 -0400

    fix import

--- examples/example02.py
+++ examples/example02.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+from timeutil import TimeUtilities
+
 def example02():
 
     """ Coord. of the subsolar point """
-
-    from datetime import datetime
-    from timeutil.timeutil import TimeUtilities
 
     dtObj = TimeUtilities()
     print(dtObj.SubSol(datetime(2004,11,21,17,0,0)))
